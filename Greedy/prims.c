6ed0299b809ebbbb922236eeff308bc5e2f9da72
Glanisha <lizacastelinoo@gmail.com>
2025-04-29 16:37:26 +0530

    ISE 2

--- Greedy/prims.c
+++ Greedy/prims.c
@@ -1,60 +1,65 @@
-#include <stdio.h>
-#include <limits.h>
+#include <stdio.h> 
 
+#define INF 9999
 #define MAX 100
 
-int main() {
-    int numberOfVertices;
-    int graph[MAX][MAX];
-    int selected[MAX];
-    int parent[MAX];
-    int minEdgeWeight[MAX];
+int v; 
+int graph[MAX][MAX];
 
-    printf("Enter number of vertices: ");
-    scanf("%d", &numberOfVertices);
+int minimum(int dist[], int visited[]){
+    int min=INF, min_index=-1;
+    
+    for(int i =0; i<v; i++){
+        if(!visited[i] && dist[i]<min){
+            min=dist[i];
+            min_index=i;
+        }
+    }
+    
+    return min_index;
+}
 
-    printf("Enter the adjacency matrix (enter 0 if no edge):\n");
-    for (int i = 0; i < numberOfVertices; i++) {
-        for (int j = 0; j < numberOfVertices; j++) {
+void prims(){
+    int dist[MAX], parent[MAX], visited[MAX];
+    for(int i=0; i<v; i++){
+        dist[i]=INF;
+        visited[i]=0;
+    }
+    
+    dist[0]=0;
+    parent[0]=-1;
+    
+    for(int i=0; i<v-1; i++){
+        int min_index=minimum(dist, visited);
+        visited[min_index]=1;
+        
+        for(int j=0; j<v; j++){
+            if(graph[min_index][j] && !visited[j] && graph[min_index][j] < dist[j]){
+                parent[j]=min_index;
+                dist[j]=graph[min_index][j];
+            }
+        }
+    }
+     int cost = 0;
+    printf("\nEdge \tWeight\n");
+    for (int i = 1; i < v; i++) {
+        printf("%d - %d \t%d\n", parent[i], i, graph[i][parent[i]]);
+        cost += graph[i][parent[i]];
+    }
+    printf("Total cost of the MST: %d\n", cost);
+}
+
+int main(){
+    printf("Enter the number of vertices:");
+    scanf("%d", &v);
+    
+    printf("Enter the adjacency matrix: ");
+for (int i = 0; i < v; i++) {
+        for (int j = 0; j < v; j++) {
             scanf("%d", &graph[i][j]);
-            if (graph[i][j] == 0) {
-                graph[i][j] = INT_MAX;
-            }
         }
     }
-
-    for (int i = 0; i < numberOfVertices; i++) {
-        minEdgeWeight[i] = INT_MAX;
-        selected[i] = 0;
-    }
-
-    minEdgeWeight[0] = 0;
-    parent[0] = -1;
-
-    for (int count = 0; count < numberOfVertices - 1; count++) {
-        int min = INT_MAX, minIndex;
-
-        for (int v = 0; v < numberOfVertices; v++) {
-            if (!selected[v] && minEdgeWeight[v] < min) {
-                min = minEdgeWeight[v];
-                minIndex = v;
-            }
-        }
-
-        selected[minIndex] = 1;
-
-        for (int v = 0; v < numberOfVertices; v++) {
-            if (graph[minIndex][v] && !selected[v] && graph[minIndex][v] < minEdgeWeight[v]) {
-                parent[v] = minIndex;
-                minEdgeWeight[v] = graph[minIndex][v];
-            }
-        }
-    }
-
-    printf("\nMinimum Spanning Tree (Edge - Weight):\n");
-    for (int i = 1; i < numberOfVertices; i++) {
-        printf("%d - %d : %d\n", parent[i], i, graph[i][parent[i]]);
-    }
-
+    
+    prims();
     return 0;
 }
